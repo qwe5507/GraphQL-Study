67ced530a394305df7ccee9b5fc3bdd3478ccb0f
qwe5507 <apvmffkdls@gmail.com>
2025-09-23 18:48:11 +0900

    add 3-3,4 example (enum, interface, input type, args)

---
+++ 3-3,4-union-interface,input-args/typedefs-resolvers/_queries.js
@@ -0,0 +1,24 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Query {
+        equipments: [Equipment]
+        equipmentAdvs: [EquipmentAdv]
+        softwares: [Software]
+        software: Software
+        supplies: [Supply]
+        givens: [Given]
+        people: [People],
+        peopleFiltered(
+            team: Int,
+            sex: Sex,
+            blood_type: BloodType,
+            from: String
+        ): [People]
+        peoplePaginated(
+            page: Int!,
+            per_page: Int!
+        ): [People]
+    }
+`
+module.exports = typeDefs
